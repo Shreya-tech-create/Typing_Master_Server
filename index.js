6b0afdc11173a2bde3756f482912ae9d96366245
Shreya_Dubey <shreyadubey0803@gmail.com>
2025-07-25 19:57:08 +0900

    Update index.js

--- index.js
+++ index.js
@@ -58,7 +58,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: "https://frontend1-k6to.onrender.com",
+  credentials: true
+}));
 app.use(express.json());
 
 // Connect to DB
