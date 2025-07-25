f873c7598e46fed5b3d1eaab19266e6b680dfdec
Shreya_Dubey <shreyadubey0803@gmail.com>
2025-07-25 18:50:08 +0900

    Update db.js

--- config/db.js
+++ config/db.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.mongo_connection);
         console.log('MongoDB connected');
     } catch (error) {
         console.error('MongoDB connection failed', error);
